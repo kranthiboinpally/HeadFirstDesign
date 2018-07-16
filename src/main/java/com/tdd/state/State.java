6457e02a2207e86e04d492c1be05bf5e98a2f124
Kranthi Boinpally <kranthi.boinpally@advance-auto.com>
2018-07-16 15:32:41 -0400

    State Design Pattern with GumballMachine

---
+++ src/main/java/com/tdd/state/State.java
@@ -0,0 +1,11 @@
+package com.tdd.state;
+
+public interface State {
+ 
+	public void insertQuarter();
+	public void ejectQuarter();
+	public void turnCrank();
+	public void dispense();
+	
+	public void refill();
+}
